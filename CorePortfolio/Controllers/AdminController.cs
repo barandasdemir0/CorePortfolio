8a8ac60bbbe5f658e27fec280e93ce9c3f28d7b0
Baran <barandasdemir.bd@gmail.com>
2025-08-04 18:23:18 +0300

    adminTemplateEditing

---
+++ CorePortfolio/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CorePortfolio.Controllers
+{
+    public class AdminController : Controller
+    {
+        public PartialViewResult PartialSideBar()
+        {
+            return PartialView();
+        }
+    }
+}
