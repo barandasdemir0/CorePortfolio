27a32db467a27bebd4ad3f7a446541c1a9884feb
Baran <barandasdemir.bd@gmail.com>
2025-08-05 15:29:54 +0300

    featura_about

--- CorePortfolio/Controllers/FeatureController.cs
+++ CorePortfolio/Controllers/FeatureController.cs
@@ -1,12 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CorePortfolio.Controllers
 {
     public class FeatureController : Controller
     {
-        public IActionResult Index()
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal());
+        public IActionResult Index(int id)
         {
-            return View();
+            var values = featureManager.TGetByID(1);
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult Index(Feature feature)
+        {
+            featureManager.TUpdate(feature);
+            return RedirectToAction("Index","Default");
         }
     }
 }
---
+++ CorePortfolio/Controllers/ServiceController.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePortfolio.Controllers
+{
+    public class ServiceController : Controller
+    {
+        ServiceManager serviceManager = new ServiceManager(new EfServiceDal());
+        public IActionResult Index()
+        {
+            var values = serviceManager.TGetList();
+            return View(values);
+        }
+
+
+        [HttpGet]
+        public IActionResult ServiceInsert()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult ServiceInsert(Service service)
+        {
+            serviceManager.TAdd(service);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ServiceDelete(int id)
+        {
+            var detectedValues = serviceManager.TGetByID(id);
+            serviceManager.TDelete(detectedValues);
+            return RedirectToAction("Index");
+        }
+
+
+
+        [HttpGet]
+        public IActionResult ServiceUpdate(int id)
+        {
+            var detectedValues = serviceManager.TGetByID(id);
+            return View(detectedValues);
+        }
+        [HttpPost]
+        public IActionResult ServiceUpdate(Service service)
+        {
+            serviceManager.TUpdate(service);
+            return RedirectToAction("Index");
+        }
+    }
+}
