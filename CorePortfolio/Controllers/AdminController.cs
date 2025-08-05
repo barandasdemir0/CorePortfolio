b5ae4a702246494eb0014606eea8b082add4c5af
Baran <barandasdemir.bd@gmail.com>
2025-08-05 10:50:24 +0300

    skill part over

--- CorePortfolio/Controllers/AdminController.cs
+++ CorePortfolio/Controllers/AdminController.cs
@@ -8,5 +8,24 @@ namespace CorePortfolio.Controllers
         {
             return PartialView();
         }
+
+        public PartialViewResult PartialFooter()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialNavbar()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult PartialHead()
+        {
+            return PartialView();
+        }
+        public PartialViewResult PartialScript()
+        {
+            return PartialView();
+        }
     }
 }
--- CorePortfolio/Controllers/SkillController.cs
+++ CorePortfolio/Controllers/SkillController.cs
@@ -1,12 +1,54 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CorePortfolio.Controllers
 {
     public class SkillController : Controller
     {
+
+        SkillManager skillManager = new SkillManager(new EfSkillDal());
         public IActionResult Index()
+        {
+            var values = skillManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult SkillInsert()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult SkillInsert(Skill skill) 
+        {
+            skillManager.TAdd(skill);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult SkillDelete(int id)
+        {
+            var detectedValues = skillManager.TGetByID(id);
+            skillManager.TDelete(detectedValues);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult SkillUpdate(int id)
+        {
+            var detectedValues = skillManager.TGetByID(id);
+            return View(detectedValues);
+        }
+
+        [HttpPost]
+        public IActionResult SkillUpdate(Skill skill)
+        {
+            skillManager.TUpdate(skill);
+            return RedirectToAction("Index");
+        }
+
+
     }
 }
