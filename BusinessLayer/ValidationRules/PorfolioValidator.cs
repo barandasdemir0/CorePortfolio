36e71f46b93f8024412cefd63fa14ff4557cdf2e
Baran <barandasdemir.bd@gmail.com>
2025-08-05 14:19:58 +0300

    fluentValidationOVER

---
+++ BusinessLayer/ValidationRules/PorfolioValidator.cs
@@ -0,0 +1,21 @@
+﻿using EntityLayer.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.ValidationRules
+{
+    public class PorfolioValidator:AbstractValidator<Portfolio>
+    {
+        public PorfolioValidator()
+        {
+            RuleFor(x => x.ProjectName).NotEmpty().WithMessage("Proje Adı Boş geçilemez");
+            RuleFor(x => x.ProjectName).MinimumLength(5).WithMessage("Proje Adı en az 5 karakter olmalı.");
+            RuleFor(x => x.ProjectName).MaximumLength(55).WithMessage("Proje Adı en fazla 5 karakter olmalı.");
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Fiyat Alanı Boş geçilemez");
+        }
+    }
+}
--- CorePortfolio/Controllers/PortfolioController.cs
+++ CorePortfolio/Controllers/PortfolioController.cs
@@ -1,6 +1,8 @@
 ﻿using BusinessLayer.Concrete;
+using BusinessLayer.ValidationRules;
 using DataAccessLayer.EntityFramework;
 using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CorePortfolio.Controllers
@@ -23,9 +25,64 @@ namespace CorePortfolio.Controllers
         [HttpPost]
         public ActionResult PortfolioInsert(Portfolio portfolio)
         {
-            portfolio.Status = true;
-            portfolioManager.TAdd(portfolio);
+            PorfolioValidator validationRules = new PorfolioValidator();//tanımladığımız class klasörde
+            ValidationResult validationResult = validationRules.Validate(portfolio);// validasyona bak portfoliodan gelen verilere validasyondan geçiyormu
+            if (validationResult.IsValid) // eğer kurallar geçiyorsa
+            {
+                portfolio.Status = true;
+                portfolioManager.TAdd(portfolio);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);// modelden hatayı getir
+                }
+                
+            }
+            return View();
+
+        }
+
+        public IActionResult PortfolioDelete(int id)
+        {
+            var detectedValues = portfolioManager.TGetByID(id);
+            portfolioManager.TDelete(detectedValues);
             return RedirectToAction("Index");
         }
+
+
+        [HttpGet]
+        public ActionResult PortfolioUpdate(int id)
+        {
+           var values =  portfolioManager.TGetByID(id);
+            return View(values);
+        }
+        [HttpPost]
+        public ActionResult PortfolioUpdate(Portfolio portfolio)
+        {
+            PorfolioValidator validationRules = new PorfolioValidator();//tanımladığımız class klasörde
+            ValidationResult validationResult = validationRules.Validate(portfolio);// validasyona bak portfoliodan gelen verilere validasyondan geçiyormu
+            if (validationResult.IsValid) // eğer kurallar geçiyorsa
+            {
+                portfolio.Status = true;
+                portfolioManager.TUpdate(portfolio);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in validationResult.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);// modelden hatayı getir
+                }
+
+            }
+            return View();
+
+        }
+
+
+
     }
 }
