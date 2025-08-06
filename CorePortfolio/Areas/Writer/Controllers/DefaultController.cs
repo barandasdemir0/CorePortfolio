cf6260f433a2feb84358aa120273c4b459b01393
Baran <barandasdemir.bd@gmail.com>
2025-08-06 15:35:15 +0300

    area

---
+++ CorePortfolio/Areas/Writer/Controllers/DefaultController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CorePortfolio.Areas.Writer.Controllers
+{
+    public class DefaultController : Controller
+    {
+
+        [Area("Writer")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- CorePortfolio/Program.cs
+++ CorePortfolio/Program.cs
@@ -17,7 +17,12 @@ namespace CorePortfolio
                 app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
+
+
+
             }
+
+
 
             app.UseHttpsRedirection();
             app.UseRouting();
@@ -29,6 +34,11 @@ namespace CorePortfolio
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}")
                 .WithStaticAssets();
+
+            app.MapControllerRoute(
+                 name: "areas",
+                 pattern: "{area:exists}/{controller=Default}/{action=Index}/{id?}");
+    
 
             app.Run();
         }
