6369f42e2d15a7634138fbcb311003345bab9626
Baran <barandasdemir.bd@gmail.com>
2025-08-08 21:55:33 +0300

    ajax_start

---
+++ BusinessLayer/Concrete/WriterUserManager.cs
@@ -0,0 +1,51 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+    public class WriterUserManager : IWriterService
+    {
+        IWriteDal _writeDal;
+
+        public WriterUserManager(IWriteDal writeDal)
+        {
+            _writeDal = writeDal;
+        }
+
+        public void TAdd(WriterUser t)
+        {
+            _writeDal.Insert(t);
+        }
+
+        public void TDelete(WriterUser t)
+        {
+            _writeDal.Delete(t);
+        }
+
+        public WriterUser TGetByID(int id)
+        {
+           return _writeDal.GetByID(id);
+        }
+
+        public List<WriterUser> TGetList()
+        {
+           return _writeDal.GetList();
+        }
+
+        public List<WriterUser> TGetListByFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void TUpdate(WriterUser t)
+        {
+            _writeDal.Update(t);
+        }
+    }
+}
---
+++ CorePortfolio/Controllers/WriterUserController.cs
@@ -0,0 +1,23 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace CorePortfolio.Controllers
+{
+    public class WriterUserController : Controller
+    {
+        WriterUserManager userManager = new WriterUserManager(new EfWriterDal());
+        public IActionResult Index()
+        {
+          
+            return View();
+        }
+        public IActionResult GetWriterUserList()
+        {
+            var users = JsonConvert.SerializeObject(userManager.TGetList());
+            return Json(users);
+        }
+    }
+}
