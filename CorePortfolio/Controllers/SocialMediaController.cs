8e21dea18dd6e5ed55dbb262572efcc4227bfa6c
Baran <barandasdemir.bd@gmail.com>
2025-08-08 16:46:33 +0300

    social_media

---
+++ CorePortfolio/Controllers/SocialMediaController.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePortfolio.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        SocialMediaManager mediaManager = new SocialMediaManager(new EfSocialMediaDal());
+        public IActionResult Index()
+        {
+            var values = mediaManager.TGetList();
+            return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddSocialMedia()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddSocialMedia(SocialMedia socialMedia)
+        {
+            socialMedia.SocialMediaStatus = true;
+            mediaManager.TAdd(socialMedia);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult DeleteSocialMedia(int id)
+        {
+            var values = mediaManager.TGetByID(id);
+            mediaManager.TDelete(values);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult UpdateSocialMedia(int id)
+        {
+           var values = mediaManager.TGetByID(id); 
+            return View(values);
+        }
+
+        [HttpPost]
+        public IActionResult UpdateSocialMedia(SocialMedia socialMedia)
+        {
+            socialMedia.SocialMediaStatus = true;
+            mediaManager.TUpdate(socialMedia);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CorePortfolio/Controllers/TestimonialController.cs
@@ -0,0 +1,52 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CorePortfolio.Controllers
+{
+    public class TestimonialController : Controller
+    {
+        TestimonialManager testimonialManager = new TestimonialManager(new EfTestimonialDal());
+        public IActionResult Index()
+        {
+            var values = testimonialManager.TGetList();
+            return View(values);
+        }
+
+        public IActionResult TestimonialDelete(int id)
+        {
+            var value = testimonialManager.TGetByID(id);
+            testimonialManager.TDelete(value);
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult TestimonialInsert()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult TestimonialInsert(Testimonial testimonial)
+        {
+            testimonialManager.TAdd(testimonial);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult TestimonialUpdate(int id)
+        {
+            var value = testimonialManager.TGetByID(id);
+            return View(value);
+        }
+
+        [HttpPost]
+        public IActionResult TestimonialUpdate(Testimonial testimonial)
+        {
+            testimonialManager.TUpdate(testimonial);
+            return RedirectToAction("Index");
+        }
+    }
+}
